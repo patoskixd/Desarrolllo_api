48aec1d8c26e1532440fa35a3052f2c50202e12a
Joaquin Aguilar <111210035+Juaker1@users.noreply.github.com>
2023-05-28 19:17:36 -0400

    Add files via upload
    Ultima actualizacion, Dia 5/28/2023 a las 7.16

---
+++ Actualización 28-5-2023, 7.16/app/Registrarse/page.jsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { NavBar } from '../components/NavBar';
+import { useRouter } from 'next/navigation';
+
+export default function Register() {
+    const router = useRouter();
+    return(
+        <div>
+        <NavBar/>
+        <div id="container">
+            <form>
+                <h1>Ingrese los datos solicitados para registrarse</h1>
+                <br/>
+                <label htmlFor="name"></label>
+                <input type="text" id="nombres" name="nombres" placeholder="Ingrese sus nombres"/>
+
+                <label htmlFor="name"></label>
+                <input type="text" id="apellidos" name="apellidos" placeholder="Ingrese sus apellidos"/>
+
+                <label htmlFor="email"></label>
+                <input type="email" id="email" name="email" placeholder="Ingrese su correo"/>
+
+                <label htmlFor="password"></label>
+                <input type="password" id="passwordR" name="passwordR" placeholder="Ingrese su contraseña"/>
+
+                <button id='boton1'>Registrarse</button>
+            </form>
+        </div>
+        </div>
+    );
+}
